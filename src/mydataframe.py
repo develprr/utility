29631b9560556f939ae31c7ef4c88cad79d581fb
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-08-13 13:33:00 +0300

    added indexing

--- src/mydataframe.py
+++ src/mydataframe.py
@@ -8,15 +8,30 @@ class MyDataFrame(BaseModel):
 
   model_config = ConfigDict(arbitrary_types_allowed=True)
   
+  dataframe: pandas.core.frame.DataFrame
+  
+  
+  @staticmethod
+  @validate_call(config=dict(arbitrary_types_allowed=True))
+  def new(dataframe: pandas.core.frame.DataFrame):
+    return MyDataFrame(**{
+      'dataframe': dataframe
+    })
   @staticmethod
   @validate_call
   def create_from_dict(dictionary: dict):
-      return pandas.DataFrame(dictionary)
-      
-  def __init__(self, data):
-    self.data = data
-
-
+    dataframe = pandas.DataFrame(dictionary)
+    return MyDataFrame.new(dataframe)
+  
+  @validate_call
+  def add_index(self, index: list[str]):
+    indexed_dataframe = pandas.DataFrame(self.dataframe, index)
+    self.dataframe = indexed_dataframe
+    return self.dataframe
+    
+  def print(self):
+    print(dataframe)
+    
 def test_create_from_dict__fail_on_wrong_parameter_type():
   try:
     dataframe = MyDataFrame.create_from_dict([1,2,3])
@@ -31,5 +46,14 @@ def test_create_from_dict__pass_when_created_from_dict():
       'oranges': [0, 3, 7, 2]
   }
   assert(type(data)) == dict
-  dataframe = MyDataFrame.create(data)
-  assert(type(dataframe)) == pandas.core.frame.DataFrame
+  dataframe = MyDataFrame.create_from_dict(data)
+  assert(type(dataframe)) == MyDataFrame
+
+def test_add_index__pass_on_string_list():
+  data = {
+      'apples': [3, 2, 0, 1], 
+      'oranges': [0, 3, 7, 2]
+  }
+  dataframe = MyDataFrame.create_from_dict(data)
+  dataframe = dataframe.add_index(["June", "Robert", "Lily", "David"])
+  print(dataframe)
