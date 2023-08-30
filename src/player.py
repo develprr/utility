968441e25eb3b67ecd1fe5e4813fdb221480fb77
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2023-08-30 21:58:59 +0300

    Wrote integration tests to verify that find_all() and delete_all() methods work.

--- src/player.py
+++ src/player.py
@@ -25,3 +25,18 @@ def test_json():
   player = Player.new("21", "Ronaldinho Gaucho")
   json = player.to_json()
   assert(json == { "_id": "21", "name": "Ronaldinho Gaucho" })
+
+def test_find_all():
+  Player.delete_all()
+  Player.new("21", "Ronaldinho Gaucho").insert()
+  Player.new("10", "Diego Maradona").insert()
+  found_players = Player.find_all()
+  assert(len(found_players) == 2)
+  
+def test_delete_all():
+  Player.delete_all()
+  Player.new("21", "Ronaldinho Gaucho").insert()
+  Player.new("10", "Diego Maradona").insert()
+  Player.delete_all()
+  found_players = Player.find_all()
+  assert(len(found_players) == 0)
