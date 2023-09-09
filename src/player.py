559d2ac4b3576335d79be71ca6b501e178f9dab6
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2023-09-09 12:04:46 +0300

    Wrote integration tests to verify that mongodb documents are correctly serialized to Pydantic objects.

--- src/player.py
+++ src/player.py
@@ -1,6 +1,6 @@
 # (C) Heikki Kupiainen 2023    
 
-from pydantic import StrictStr, validate_call
+from pydantic import StrictStr, validate_call, create_model
 from msmodel import MSModel
   
 class Player(MSModel):
@@ -15,6 +15,9 @@ class Player(MSModel):
       'id': id,
       'name': name
     })
+    
+  def get_description(self):
+    return f"{self.name}: {self.id}"
       
 def test_new():
   player = Player.new("21", "Ronaldinho Gaucho")
@@ -40,3 +43,15 @@ def test_delete_all():
   Player.delete_all()
   found_players = Player.find_all()
   assert(len(found_players) == 0)
+  
+def test_create_dynamically():
+  player = eval("Player")(**{ "id": "21", "_id": "21", "name": "Ronaldinho Gaucho" })
+  description = player.get_description()
+  assert(description == "Ronaldinho Gaucho: 21")
+
+def test_create_from_database():
+  Player.delete_all()
+  Player.new("21", "Ronaldinho Gaucho").insert()
+  player = Player.find_one({ "_id": "21" })
+  description = player.get_description()
+  assert(description == "Ronaldinho Gaucho: 21")
